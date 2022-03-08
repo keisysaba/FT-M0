688f02157dd4aced373d26c26265ed3d4cf8d591
keisy <kd.72002@outlook.com>
2022-03-08 12:11:03 -0500

    creo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -234,10 +234,9 @@ function esVocal(letra){
   if (letra.length > 1){
     return "Dato incorrecto";
   }
-  if (letra === a || letra === e || letra === i || letra === o || letra === u){
+  if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
     return "Es vocal";
   }
-  return "Dato incorrecto";
 }
 
 
